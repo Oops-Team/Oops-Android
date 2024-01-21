57c5cf779e4909690bbd930509e09e729e3e111b
Yoonji Lee <yooonji08@naver.com>
2024-01-21 18:44:43 +0900

    Feat: 회원가입 화면 edittext 입력 이벤트 구현
    - 이메일, 비밀번호, 비밀번호 재확인

--- Oops-Android/app/src/main/java/com/example/oops_android/ui/Login/SignUp2Activity.kt
+++ Oops-Android/app/src/main/java/com/example/oops_android/ui/Login/SignUp2Activity.kt
@@ -1,5 +1,9 @@
 package com.example.oops_android.ui.Login
 
+import android.content.res.ColorStateList
+import android.view.View
+import androidx.core.content.ContextCompat
+import com.example.oops_android.R
 import com.example.oops_android.databinding.ActivitySignUp2Binding
 import com.example.oops_android.ui.BaseActivity
 import com.example.oops_android.utils.ButtonUtils
@@ -14,6 +18,10 @@ class SignUp2Activity: BaseActivity<ActivitySignUp2Binding>(ActivitySignUp2Bindi
     private var isPwdMask: Boolean = false // 비밀번호 mask on/off 변수
     private var isPwdCheckMask: Boolean = false // 비밀번호 재확인 mask on/off 변수
 
+    private var isEmailValid: Boolean = false // 이메일 유효성 여부
+    private var isPwdValid: Boolean = false // 비밀번호 유효성 여부
+    private var isPwdCheckValid: Boolean = false // 비밀번호 재확인 유효성 여부
+
     override fun beforeSetContentView() {
     }
 
@@ -22,6 +30,34 @@ class SignUp2Activity: BaseActivity<ActivitySignUp2Binding>(ActivitySignUp2Bindi
         // 비밀번호 mask의 스타일 지정
         binding.edtSignUp2Pwd.transformationMethod = CustomPasswordTransformationMethod()
         binding.edtSignUp2PwdCheck.transformationMethod = CustomPasswordTransformationMethod()
+
+        // 이메일 중복 검사 버튼 클릭 이벤트
+        binding.tvSignUp2OverlapBtn.setOnClickListener {
+            // 이메일 형식이 맞다면
+            if (EditTextUtils.emailRegex(binding.edtSignUp2Email.text.toString().trim())) {
+                // TODO: 중복 확인 검사하기
+
+                // 중복이 아니라면
+                isEmailValid = true
+                binding.edtSignUp2Email.isEnabled = false // 이메일 입력 막기
+                binding.tvSignUp2EmailAlert.text = "사용 가능한 이메일이에요!"
+                binding.tvSignUp2EmailAlert.setTextColor(ContextCompat.getColor(applicationContext, R.color.Main_500))
+                binding.edtSignUp2Email.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(applicationContext, R.color.Main_500))
+                binding.ivSignUp2EmailAlert.visibility = View.VISIBLE
+                binding.tvSignUp2OverlapBtn.visibility = View.INVISIBLE
+                checkValid() // 모든 유효성 확인
+
+                /*
+                // 중복 이라면
+                isEmailValid = false
+                binding.tvSignUp2EmailAlert.text = "이미 사용 중인 이메일이에요"
+                binding.tvSignUp2EmailAlert.setTextColor(ContextCompat.getColor(applicationContext, R.color.Red_Medium))
+                binding.edtSignUp2Email.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(applicationContext, R.color.Red_Dark))
+                binding.tvSignUp2OverlapBtn.visibility = View.VISIBLE
+                binding.ivSignUp2EmailAlert.visibility = View.INVISIBLE
+                */
+            }
+        }
 
         // 비밀번호 - mask on/off 클릭 이벤트
         binding.iBtnSignUp2PwdToggle.setOnClickListener {
@@ -45,6 +81,96 @@ class SignUp2Activity: BaseActivity<ActivitySignUp2Binding>(ActivitySignUp2Bindi
                     ButtonUtils().setOnClickToggleBtn(binding.iBtnSignUp2PwdCheckToggle, isPwdCheckMask, binding.edtSignUp2PwdCheck)
                     true
                 }
+        }
+
+        // 비밀번호 - 입력 조건 검사
+        binding.edtSignUp2Pwd.onTextChanged {
+            // 비밀번호 재확인 필드가 작성되어 있다면
+            if (binding.edtSignUp2PwdCheck.text.toString().isNotBlank()) {
+                // 두 개의 필드가 다르다면
+                if (binding.edtSignUp2Pwd.text.toString() != binding.edtSignUp2PwdCheck.text.toString()) {
+                    isPwdCheckValid = false
+                    binding.tvSignUp2PwdCheckAlert.visibility = View.VISIBLE
+                    binding.tvSignUp2PwdCheckAlert.text = "비밀번호가 일치하지 않습니다"
+                    binding.tvSignUp2PwdCheckAlert.setTextColor(ContextCompat.getColor(applicationContext, R.color.Red_Medium))
+                    binding.edtSignUp2PwdCheck.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(applicationContext, R.color.Red_Dark))
+                }
+                // 두 개의 필드가 같다면
+                else {
+                    isPwdCheckValid = true
+                    binding.tvSignUp2PwdCheckAlert.visibility = View.VISIBLE
+                    binding.tvSignUp2PwdCheckAlert.text = "비밀번호가 일치합니다"
+                    binding.tvSignUp2PwdCheckAlert.setTextColor(ContextCompat.getColor(applicationContext, R.color.Main_500))
+                    binding.edtSignUp2PwdCheck.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(applicationContext, R.color.Main_500))
+                    checkValid()
+                }
+            }
+
+            // 15자 이상 이라면
+            if (binding.edtSignUp2Pwd.length() >= 15) {
+                isPwdValid = false
+                // 알럿 텍스트 출력
+                binding.tvSignUp2PwdAlert.text = "비밀번호는 15자까지 입력 가능해요"
+                binding.edtSignUp2Pwd.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(applicationContext, R.color.Red_Dark))
+                binding.tvSignUp2PwdAlert.visibility = View.VISIBLE
+            }
+            // 영문, 숫자, 특수 문자가 없다면
+            else if (!EditTextUtils.passwordRegex(binding.edtSignUp2Pwd.text.toString())) {
+                isPwdValid = false
+                // 알럿 텍스트 출력
+                binding.tvSignUp2PwdAlert.text = "영문, 숫자, 특수문자를 포함해야 해요"
+                binding.edtSignUp2Pwd.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(applicationContext, R.color.Red_Dark))
+                binding.tvSignUp2PwdAlert.visibility = View.VISIBLE
+            }
+            else {
+                // 조건에 부합하다면
+                if (EditTextUtils.passwordRegex(binding.edtSignUp2Pwd.text.toString())) {
+                    isPwdValid = true
+                    binding.tvSignUp2PwdAlert.visibility = View.INVISIBLE
+                    binding.ivSignUp2EmailAlert.visibility = View.VISIBLE
+                    binding.edtSignUp2Pwd.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(applicationContext, R.color.Main_500))
+                    checkValid() // 모든 유효성 확인
+                }
+            }
+        }
+
+        // 비밀번호 재확인 - 입력 조건 검사
+        binding.edtSignUp2PwdCheck.onTextChanged {
+            // 입력이 되어있는 상태라면
+            if (binding.edtSignUp2PwdCheck.text.toString().isNotBlank()) {
+                // 비밀번호와 비밀번호 재확인 필드가 일치하지 않는다면
+                if (binding.edtSignUp2Pwd.text.toString() != binding.edtSignUp2PwdCheck.text.toString()) {
+                    isPwdCheckValid = false
+                    binding.tvSignUp2PwdCheckAlert.visibility = View.VISIBLE
+                    binding.tvSignUp2PwdCheckAlert.text = "비밀번호가 일치하지 않습니다"
+                    binding.tvSignUp2PwdCheckAlert.setTextColor(ContextCompat.getColor(applicationContext, R.color.Red_Medium))
+                    binding.edtSignUp2PwdCheck.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(applicationContext, R.color.Red_Dark))
+                }
+                // 일치한다면
+                else {
+                    isPwdCheckValid = true
+                    binding.tvSignUp2PwdCheckAlert.text = "비밀번호가 일치합니다"
+                    binding.tvSignUp2PwdCheckAlert.setTextColor(ContextCompat.getColor(applicationContext, R.color.Main_500))
+                    binding.edtSignUp2PwdCheck.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(applicationContext, R.color.Main_500))
+                    checkValid() // 모든 유효성 확인
+                }
+            }
+            // 입력이 안 되어있는 상태라면
+            else {
+                binding.tvSignUp2PwdCheckAlert.visibility = View.INVISIBLE
+                binding.edtSignUp2PwdCheck.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(applicationContext, R.color.Gray_300))
+            }
+        }
+    }
+
+    // 이메일, 비밀번호, 비밀번호 재확인 유효성 검사
+    private fun checkValid() {
+        // TODO: 개인정보 수집 및 이용 동의 바텀 시트띄우기
+        if (isEmailValid && isPwdValid && isPwdCheckValid) {
+            showToast("모두 만족!")
+        }
+        else {
+            showToast("1.$isEmailValid, 2.$isPwdValid, 3.$isPwdCheckValid 불만족!")
         }
     }
 }
--- Oops-Android/app/src/main/java/com/example/oops_android/utils/EditTextUtils.kt
+++ Oops-Android/app/src/main/java/com/example/oops_android/utils/EditTextUtils.kt
@@ -2,8 +2,28 @@ package com.example.oops_android.utils
 
 import android.text.method.PasswordTransformationMethod
 import android.view.View
+import java.util.regex.Pattern
 
 // EditText와 관련한 함수 및 변수를 저장한 파일
+class EditTextUtils {
+    companion object {
+        // 이메일 정규식
+        private const val emailPattern: String = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"
+
+        // 비밀번호 패턴(영어, 숫자, 일부 특수문자 허용)
+        private const val passwordPattern: String ="^(?=.*[a-z])(?=.*[0-9])(?=.*[?!*~@#%^])[a-zA-Z0-9?!*~@#%^]{8,15}$"
+
+        // 이메일 정규식 확인
+        fun emailRegex(email: String): Boolean {
+            return email.matches(emailPattern.toRegex())
+        }
+
+        // 비밀번호 패턴 일치 확인
+        fun passwordRegex(password: String): Boolean {
+            return password.matches(passwordPattern.toRegex())
+        }
+    }
+}
 
 // 기본 hide 텍스트를 '●'로 커스텀하는 클래스
 class CustomCharSequence(private val source: CharSequence): CharSequence {
