5fb1da98bfcac7a5aefc977fd4cd92d5a747c619
Yoonji Lee <yooonji08@naver.com>
2024-01-24 22:21:34 +0900

    Feat: 개인정보 수집 바텀 시트 화면 구현
    - enable 버튼, disable 버튼 style로 제작 완료

---
+++ Oops-Android/app/src/main/java/com/example/oops_android/ui/Login/TermsBottomSheetFragment.kt
@@ -0,0 +1,15 @@
+package com.example.oops_android.ui.Login
+
+import com.example.oops_android.databinding.FragmentTermsBottomSheetBinding
+import com.example.oops_android.ui.Base.BaseFragment
+
+// 개인정보 수집 및 이용 동의 약관 바텀 시트
+class TermsBottomSheetFragment : BaseFragment<FragmentTermsBottomSheetBinding>(FragmentTermsBottomSheetBinding::inflate) {
+    override fun initViewCreated() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initAfterBinding() {
+        TODO("Not yet implemented")
+    }
+}
