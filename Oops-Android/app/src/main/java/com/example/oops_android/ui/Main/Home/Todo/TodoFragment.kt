0508850ff9f277647829ac2baf970c43fa517c15
Yoonji Lee <yooonji08@naver.com>
2024-01-26 21:39:12 +0900

    Refactor: 일정 추가 화면 이름 및 폴더 경로 변경

---
+++ Oops-Android/app/src/main/java/com/example/oops_android/ui/Main/Home/Todo/TodoFragment.kt
@@ -0,0 +1,14 @@
+package com.example.oops_android.ui.Main.Home.Todo
+
+import com.example.oops_android.databinding.FragmentTodoBinding
+import com.example.oops_android.ui.Base.BaseFragment
+
+// 일정 추가 & 수정 화면
+class TodoFragment: BaseFragment<FragmentTodoBinding>(FragmentTodoBinding::inflate) {
+    override fun initViewCreated() {
+    }
+
+    override fun initAfterBinding() {
+    }
+
+}
