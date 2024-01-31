d518afaa5b71c080d47b07c972c0769068a5c229
Yoonji Lee <yooonji08@naver.com>
2024-01-31 23:09:30 +0900

    Feat: 마이페이지, 공지사항, 공지사항 상세 화면 UI 구현
    - UI 디자인 구현 완료
    - 버튼 클릭 이벤트(네비게이션) 구현 완료

--- Oops-Android/app/src/main/java/com/example/oops_android/ui/MyPage/MyPageFragment.kt
+++ Oops-Android/app/src/main/java/com/example/oops_android/ui/MyPage/MyPageFragment.kt
@@ -5,56 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavDirections
+import androidx.navigation.findNavController
 import com.example.oops_android.R
+import com.example.oops_android.databinding.FragmentMyPageBinding
+import com.example.oops_android.ui.Base.BaseFragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+// 마이 페이지 화면
+class MyPageFragment: BaseFragment<FragmentMyPageBinding>(FragmentMyPageBinding::inflate) {
+    override fun initViewCreated() {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MyPageFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class MyPageFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun initAfterBinding() {
+        // 공지형 팝업을 클릭한 경우
+        binding.lLayoutMyPageCommentNotice.setOnClickListener {
+            // 공지사항 화면으로 이동
+            val actionToNotice: NavDirections = MyPageFragmentDirections.actionMyPageFrmToNoticeFrm()
+            view?.findNavController()?.navigate(actionToNotice)
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_page, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MyPageFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MyPageFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
