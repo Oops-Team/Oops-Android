136cc0e424fea68fcea84b0680a8990eda82dc1a
Yoonji Lee <yooonji08@naver.com>
2024-02-04 12:37:18 +0900

    Feat: 계정 관리 화면 UI 구현

---
+++ Oops-Android/app/src/main/java/com/example/oops_android/ui/MyPage/AccountFragment.kt
@@ -0,0 +1,22 @@
+package com.example.oops_android.ui.MyPage
+
+import androidx.navigation.findNavController
+import com.example.oops_android.R
+import com.example.oops_android.databinding.FragmentAccountBinding
+import com.example.oops_android.ui.Base.BaseFragment
+
+// 계정 관리 화면
+class AccountFragment: BaseFragment<FragmentAccountBinding>(FragmentAccountBinding::inflate) {
+    override fun initViewCreated() {
+        mainActivity?.hideBnv(true) // bnv 숨기기
+
+        binding.toolbarAccount.tvSubToolbarTitle.text = getString(R.string.myPage_account) // 툴바 설정
+    }
+
+    override fun initAfterBinding() {
+        // 뒤로 가기 버튼 클릭
+        binding.toolbarAccount.ivSubToolbarBack.setOnClickListener {
+            view?.findNavController()?.popBackStack()
+        }
+    }
+}
--- Oops-Android/app/src/main/java/com/example/oops_android/ui/MyPage/MyPageFragment.kt
+++ Oops-Android/app/src/main/java/com/example/oops_android/ui/MyPage/MyPageFragment.kt
@@ -1,10 +1,5 @@
 package com.example.oops_android.ui.MyPage
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.NavDirections
 import androidx.navigation.findNavController
 import com.example.oops_android.R
@@ -14,7 +9,7 @@ import com.example.oops_android.ui.Base.BaseFragment
 // 마이 페이지 화면
 class MyPageFragment: BaseFragment<FragmentMyPageBinding>(FragmentMyPageBinding::inflate) {
     override fun initViewCreated() {
-
+        mainActivity?.hideBnv(false)
     }
 
     override fun initAfterBinding() {
@@ -25,6 +20,11 @@ class MyPageFragment: BaseFragment<FragmentMyPageBinding>(FragmentMyPageBinding:
             view?.findNavController()?.navigate(actionToNotice)
         }
 
+        // 계정 설정 탭을 클릭한 경우
+        binding.lLayoutMyPageAccount.setOnClickListener {
+            val actionToAccount: NavDirections = MyPageFragmentDirections.actionMyPageFrmToAccountFrm()
+            view?.findNavController()?.navigate(actionToAccount)
+        }
         // 알림 설정 탭을 클릭한 경우
         binding.lLayoutMyPageAlarm.setOnClickListener {
             val actionToAlert: NavDirections = MyPageFragmentDirections.actionMyPageFrmToAlertFrm()
