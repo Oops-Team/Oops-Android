bd62fe3bdc3080651de63464a2649e38630bd24b
Yoonji Lee <yooonji08@naver.com>
2024-04-14 17:35:16 +0900

    Fix: 개인정보처리방침 바텀 시트 뒤로가기 막기 & 바텀 시트가 상단에 붙는 이슈 해결 #35

--- Oops-Android/app/src/main/java/com/oops/oops_android/ui/Login/TermsBottomSheetFragment.kt
+++ Oops-Android/app/src/main/java/com/oops/oops_android/ui/Login/TermsBottomSheetFragment.kt
@@ -1,14 +1,14 @@
 package com.oops.oops_android.ui.Login
 
 import android.app.Dialog
-import android.content.res.ColorStateList
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
-import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.content.ContextCompat
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.oops.oops_android.R
 import com.oops.oops_android.databinding.FragmentTermsBottomSheetBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
@@ -27,23 +27,26 @@ class TermsBottomSheetFragment(val itemClick: (Int) -> Unit): BottomSheetDialogF
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         super.onCreateView(inflater, container, savedInstanceState)
         mBinding = FragmentTermsBottomSheetBinding.inflate(inflater, container, false)
 
+        // 핸드폰 내의 뒤로가기 버튼 클릭 이벤트 막기
+        dialog?.setOnKeyListener { _, keyCode, event ->
+            keyCode == KeyEvent.KEYCODE_BACK && event.action == KeyEvent.ACTION_UP
+        }
+
         return binding.root
     }
-
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val dialog = super.onCreateDialog(savedInstanceState)
+        val dialog = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
 
         dialog.window?.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND) // dim 제거
-        // 화면 바깥 터치 막기 & 드래그 막기
+
         dialog.apply {
             setOnShowListener {
-                window?.findViewById<View>(com.google.android.material.R.id.touch_outside)?.setOnClickListener(null)
-                (window?.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
-                    ?.layoutParams as CoordinatorLayout.LayoutParams).behavior = null
+                setCanceledOnTouchOutside(false) // 화면 바깥 터치 시 바텀 시트 닫치지 않도록 설정
+                behavior.isDraggable = false // 드래그 시 바텀 시트 닫치지 않도록 설정
             }
         }
 
@@ -53,17 +56,23 @@ class TermsBottomSheetFragment(val itemClick: (Int) -> Unit): BottomSheetDialogF
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // 바텀 시트 높이 조절
-        dialog?.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-
         // 개인정보 수집 및 이용 동의 버튼을 클릭한 경우
         binding.ivTermsBottomSheetVital.setOnClickListener {
-            isVitalCheck = true
-            binding.ivTermsBottomSheetVital.imageTintList = ColorStateList.valueOf(ContextCompat.getColor(requireContext(), R.color.Main_500)) // 아이콘 보라색으로
+            isVitalCheck = !isVitalCheck
+            if (isVitalCheck) {
+                binding.ivTermsBottomSheetVital.setImageResource(R.drawable.ic_confirm_25) // 아이콘 바꾸기
 
-            // 버튼 색상 바꾸기
-            binding.btnTermsBottomSheetNext.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(requireContext(), R.color.Main_500))
-            binding.btnTermsBottomSheetNext.setTextColor(ContextCompat.getColor(requireContext(), R.color.White))
+                // 버튼 색상 바꾸기
+                binding.btnTermsBottomSheetNext.setTextAppearance(R.style.SemiWideButtonEnableStyle)
+                binding.btnTermsBottomSheetNext.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.Main_500))
+            }
+            else {
+                binding.ivTermsBottomSheetVital.setImageResource(R.drawable.ic_check_24) // 아이콘 바꾸기
+
+                // 버튼 색상 바꾸기
+                binding.btnTermsBottomSheetNext.setTextAppearance(R.style.SemiWideButtonDisableStyle)
+                binding.btnTermsBottomSheetNext.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.Gray_100))
+            }
         }
 
         // 개인정보 수집 및 이용 동의 텍스트를 클릭한 경우
